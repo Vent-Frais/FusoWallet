adce652858264dcbd6e6ae6b398524771b1e6a07
XuDongLiu-star <xudongliu@bitcove.vip>
2021-02-19 11:15:44 +0800

    Initial Commit

---
+++ FusoWallet/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FusoWallet
+//
+//  Created by Developer on 2021/2/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FusoWallet/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FusoWallet
+//
+//  Created by Developer on 2021/2/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
